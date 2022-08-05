0146421e2ddfd1d90e6450546174902f24c0f85d
shery <382254994@qq.com>
2022-08-05 18:44:30 +0800

    feat: 🎸 test

--- service_worker.js
+++ service_worker.js
@@ -1,6 +1,13 @@
+var SONOBI_PUSH_ACCOUNT_ID = "1040624865034";
+self.importScripts(
+  "https://sbi-push.com/sbi_service_worker_import_v1.0.4.js?accountid=" +
+    SONOBI_PUSH_ACCOUNT_ID
+);
+
 self.addEventListener("install", (event) => {
   console.log("Service worker installed");
 });
+
 self.addEventListener("activate", (event) => {
   console.log("Service worker activated");
 });
